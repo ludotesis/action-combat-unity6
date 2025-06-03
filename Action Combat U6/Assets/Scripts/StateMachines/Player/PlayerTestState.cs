4c0dd0bf61413a5e5dc5574ab7bda6dd1f004362
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-06-03 09:13:31 -0300

    PlayerTestState

---
+++ Action Combat U6/Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerTestState : PlayerBaseState
+{
+    private float timer = 3f;
+    public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        Debug.Log("Enter");
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        timer -= deltaTime;
+        Debug.Log("Tick");
+
+        if (timer <= 0)
+        {
+            stateMachine.SwitchState(new PlayerTestState(stateMachine));
+        }
+    }
+
+    public override void Exit()
+    {
+        Debug.Log("Exit");
+    }
+}
