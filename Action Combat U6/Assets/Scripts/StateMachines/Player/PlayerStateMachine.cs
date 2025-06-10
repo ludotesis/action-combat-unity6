f87b0823dbec579429c25acbec1f970a295628ec
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-06-10 09:10:02 -0300

    Player Character Controller

--- Action Combat U6/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Action Combat U6/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -5,6 +5,12 @@ public class PlayerStateMachine : StateMachine
     [field:SerializeField]
     public InputReader InputReader { get; private set; }
     
+    [field:SerializeField]
+    public CharacterController CharacterController { get; private set; }
+    
+    [field:SerializeField]
+    public float FreeLookMovementSpeed { get; private set; }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
